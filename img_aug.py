ef76a453e09a306feec72297ce902adc5e08a023
b06502162Lu <b06502162@g.ntu.edu.tw>
2021-08-16 14:57:28 +0800

    debug

--- img_aug.py
+++ img_aug.py
@@ -14,7 +14,9 @@ target_dir = datasets_root_dir + 'train_cropped_augmented/'
 #makedir(target_dir)
 folders = [os.path.join(dir, folder) for folder in next(os.walk(dir))[1]]
 target_folders = [os.path.join(target_dir, folder) for folder in next(os.walk(dir))[1]]
-print("This is folder:  ",folders)
+for i in range(10) :
+    print("This is folder ",i,  ",folders[i])
+print("final",folder[-1])
 #print("This is target:  ",target_folders)
 print("\n\n\n\n")
 
