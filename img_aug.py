8abe9a0b7757919bc3abd890038d463f0bf0ebc8
b06502162Lu <b06502162@g.ntu.edu.tw>
2021-08-16 19:08:22 +0800

    debug

--- img_aug.py
+++ img_aug.py
@@ -11,6 +11,7 @@ datasets_root_dir = './datasets/cub200_cropped/'
 dir_ = datasets_root_dir + 'train_cropped/'
 target_dir = datasets_root_dir + 'train_cropped_augmented/'
 
+"""
 folders = list()
 target_folders = list()
 for name in os.listdir(dir_) :
@@ -30,16 +31,13 @@ print("\n\n\n\n")
 #makedir(target_dir)
 folders = [os.path.join(dir, folder) for folder in next(os.walk(dir))[1]]
 target_folders = [os.path.join(target_dir, folder) for folder in next(os.walk(dir))[1]]
-for i in range(10) :
-    print("This is folder ",i," ",folders[i])
-print("final",folders[-1])
-#print("This is target:  ",target_folders)
-print("\n\n\n\n")
-"""
+
+
 
 
 
 for i in range(len(folders)):
+    print(" i ",i,"\n")
     fd = folders[i]
     tfd = target_folders[i]
 
