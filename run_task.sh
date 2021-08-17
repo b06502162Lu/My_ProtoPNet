2f73da0f86d5c40bac47535aab02723ca5741bc9
b06502162Lu <b06502162@g.ntu.edu.tw>
2021-08-17 13:36:31 +0800

    debug

--- run_task.sh
+++ run_task.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l select=1:ncpus=8:ngpus=1  
 #PBS -q ee
-source activate b06502162
+source activate env-name b06502162
 cd $PBS_O_WORKDIR
 module load cuda/cuda-10.0/x86_64
 python tensorflow-main.py
