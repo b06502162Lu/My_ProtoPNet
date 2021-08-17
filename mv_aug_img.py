36bcaea4786df456f1e3145f67285ee43ceb0dac
b06502162Lu <b06502162@g.ntu.edu.tw>
2021-08-17 11:00:00 +0800

    debug

--- mv_aug_img.py
+++ mv_aug_img.py
@@ -2,9 +2,11 @@ import os
 folders = []
 img_dir = []
 for i in os.listdir(os.getcwd()) :
+    if i == "img_aug.py" or i == "mv_aug_img.py":
+        continue
     folders.append(i)
     img_dir.append(i)
-#l
+
 for i in range(len(folders)):
     
     fd = folders[i]+"/"+img_dir[i]
@@ -13,3 +15,4 @@ for i in range(len(folders)):
     print(folders[i],"  :  ",len(fd))
     cmd = "mv "+fd+" "+"../train_cropped_augmented"
     fp = os.popen(cmd)
+print("finsish move data")
