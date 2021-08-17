af96ddf9aeff366d57ec4777f7c8c534787a4aaa
b06502162Lu <b06502162@g.ntu.edu.tw>
2021-08-17 13:39:26 +0800

    debug

--- run_task.sh
+++ run_task.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -l select=1:ncpus=8:ngpus=1  
 #PBS -q ee
-source activate env-name b06502162
+source activate /home/eegroup/eefrank/anaconda3/envs/b06502162
 cd $PBS_O_WORKDIR
 module load cuda/cuda-10.0/x86_64
-python tensorflow-main.py
+python /home/eegroup/eefrank/b06502162/My_ProtoPNet/main.py
 source deactivate
