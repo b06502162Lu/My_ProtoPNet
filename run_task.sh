18d6f8c87482883d534ec84b68921f32fa9964dd
b06502162Lu <b06502162@g.ntu.edu.tw>
2021-08-17 13:33:50 +0800

    update

--- run_task.sh
+++ run_task.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #PBS -l select=1:ncpus=8:ngpus=1  
-#PBS -q ee 
-
+#PBS -q ee
 source activate b06502162
 cd $PBS_O_WORKDIR
 module load cuda/cuda-10.0/x86_64
-python main.py
+python tensorflow-main.py
 source deactivate
