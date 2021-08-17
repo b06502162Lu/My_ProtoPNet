0265603d397391960d1e3c97e9ee16a766f67150
b06502162Lu <b06502162@g.ntu.edu.tw>
2021-08-17 13:44:59 +0800

    update

--- run_task.sh
+++ run_task.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -l select=1:ncpus=8:ngpus=1  
 #PBS -q ee
-source activate env-name /home/eegroup/eefrank/anaconda3/envs/b06502162
+conda activate b06502162
 cd $PBS_O_WORKDIR
 module load cuda/cuda-10.0/x86_64
-python /home/eegroup/eefrank/b06502162/My_ProtoPNet/main.py
-source deactivate
+python main.py
+conda deactivate
