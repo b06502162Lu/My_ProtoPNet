37c9907cd1f67c96f2e7067c9761a579cbb2b85a
b06502162Lu <b06502162@g.ntu.edu.tw>
2021-08-17 19:12:31 +0800

    update

--- run_task.sh
+++ run_task.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=8:ngpus=1  
+#PBS -l select=1:ncpus=8:ngpus=1
 #PBS -q ee
-conda activate b06502162
+source activate b06502162
 cd $PBS_O_WORKDIR
 module load cuda/cuda-10.0/x86_64
-python main.py
-conda deactivate
+python main.py ###你要跑的code
+source deactivate
