a717762902e881e21936ac1093445e60230232ae
b06502162Lu <b06502162@g.ntu.edu.tw>
2021-08-17 11:33:27 +0800

    update

--- run_task.sh
+++ run_task.sh
@@ -2,7 +2,7 @@
 #PBS -l select=1:ncpus=8:ngpus=1  
 #PBS -q ee 
 
-source activate b06502162
+source activate "b06502162"
 cd $PBS_O_WORKDIR
 module load cuda/cuda-10.0/x86_64
 python main.py ###你要跑的code
